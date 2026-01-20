3c8b57be5c50317e1d6b9e0235fb68d62fb375a1
OgnyanIgnatov <125972711+OgnyanIgnatov@users.noreply.github.com>
2026-01-20 20:48:53 +0200

    Delete web/config directory

--- web/config/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-require 'constants.php';
-
-$connection = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME, DB_PORT);
-
-if($connection->connect_error){
-    die('Database error'.$connection->connect_error);
-}
